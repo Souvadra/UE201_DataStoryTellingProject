5462304d64d6a333e1716361ff1f894634d29229
Souvadra <souvadra01@gmail.com>
2021-01-05 02:54:03 +0530

    I have modified the this fuction to take the whole input matrix at a time, Also, corrected the equation. FINGERS CROSSEDgit add SVM.m!

--- SVM.m
+++ SVM.m
@@ -1,12 +1,15 @@
-function L = SVM(actual_class, input_data, weight_matrix, bias_vector, delta, Lambda)
+function L = SVM(actual_class, input_data_matrix, weight_matrix, bias_vector, delta, Lambda)
 %{
 This is my SVM classifier to that takes the following parameters and
 hyper-parameters and returns the scalar Loss function.
 %}  
     %% Hope this works !!!
-    f = (weight_matrix * input_data') + bias_vector;
-    actual_answer = f(actual_class);
-    f(actual_class) = [];
-    L_i_vector = max(0,f+ (-actual_answer+delta));
-    L = mean(L_i_vector) + (Lambda * sumSquareOfMatrix(weight_matrix));
+    l = zeros(size(input_data_matrix,1),1);
+    for i=1:size(input_data_matrix,1)
+        f = (weight_matrix * input_data_matrix(i,:)') + bias_vector;
+        actual_answer = f(actual_class);
+        f(actual_class) = [];
+        l(i) = sum(max(0,f+ (-actual_answer+delta)));
+    end
+    L = mean(l) + (Lambda * sumSquareOfMatrix(weight_matrix));
 end
