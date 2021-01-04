e580ebdfe667f4457fc1e38573a32dc2682f0778
Souvadra <souvadra01@gmail.com>
2021-01-05 04:09:55 +0530

    this computes the numerical gradient of the SVM function, as per the rule I observed inside the lecture. The method is actually quite slow

---
+++ evaluateSVMNumericalGradient.m
@@ -0,0 +1,17 @@
+function dW = evaluateSVMNumericalGradient(actual_class, input_data_matrix, weight_matrix, bias_vector, delta, Lambda)
+%{
+This function implements the numericala gradient of a function f at a
+vector input point x. 
+This function will probably be very slow and is gonna take a lot of time. 
+%}
+    dW = zeros(size(weight_matrix));
+    h = 0.0001;
+    for row=1:size(weight_matrix,1)
+        for col=1:size(weight_matrix,2)
+            this_weight_matrix = weight_matrix;
+            this_weight_matrix(row,col) = this_weight_matrix(row,col) + h;
+            now_loss = SVM(actual_class, input_data_matrix, this_weight_matrix, bias_vector, delta, Lambda);
+            dW(row,col) = now_loss;
+        end
+    end
+end
