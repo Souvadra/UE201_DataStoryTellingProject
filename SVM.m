b2f9f406c548d1d7d36d18af2197901288d95d13
Souvadra <souvadra01@gmail.com>
2021-01-05 03:42:29 +0530

    correct a silly mistake in the function

--- SVM.m
+++ SVM.m
@@ -1,4 +1,4 @@
-function L = SVM(actual_class, input_data_matrix, weight_matrix, bias_vector, delta, Lambda)
+function L = SVM(class_label_vector, input_data_matrix, weight_matrix, bias_vector, delta, Lambda)
 %{
 This is my SVM classifier to that takes the following parameters and
 hyper-parameters and returns the scalar Loss function.
@@ -7,8 +7,8 @@ hyper-parameters and returns the scalar Loss function.
     l = zeros(size(input_data_matrix,1),1);
     for i=1:size(input_data_matrix,1)
         f = (weight_matrix * input_data_matrix(i,:)') + bias_vector;
-        actual_answer = f(actual_class);
-        f(actual_class) = [];
+        actual_answer = f(class_label_vector(i));
+        f(class_label_vector(i)) = [];
         l(i) = sum(max(0,f+ (-actual_answer+delta)));
     end
     L = mean(l) + (Lambda * sumSquareOfMatrix(weight_matrix));
