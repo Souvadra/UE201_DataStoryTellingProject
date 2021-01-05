94deaf6fcb04a5aa0c5dcae292924ce2b4c514b0
Souvadra <souvadra01@gmail.com>
2021-01-06 02:56:56 +0530

    This function show the pictorial representation of the weight_parameters.

---
+++ showTemplateImagesOfCurrentWeightMatrix.m
@@ -0,0 +1,11 @@
+function showTemplateImagesOfCurrentWeightMatrix(W,labelNames)
+%{
+This function takes the weight_matrix as an input and outputs the sameple
+pictures, which will give minimium loss for these weights.
+%}
+for i=1:size(W,1)
+    figure()
+    image(makeNormalisedFigureMatrixFromDataArray(W(i,:)))
+    title(string(labelNames(i)));
+end
+end
